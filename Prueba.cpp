22ac8300125a071affa9235fc225475a7445c7e1
DanielKyjas <116916840+DanielKyjas@users.noreply.github.com>
2022-11-29 18:47:28 -0600

    Update Prueba.cpp

--- Prueba.cpp
+++ Prueba.cpp
@@ -24,37 +24,48 @@ public:
 void imprimir(nodo* n)
 {
     //Algoritmo de batalla
-    while (n != NULL)
+    do
     {
-        if (n->suerte > n->next->suerte) {
-            n->power = rand() % 51;
-            if (n->power < 11) {
-                std::cout << n->nombre << " Ha encontrado un artefacto magico, ahora su ataque se duplica\n";
-                n->ataque * 2;
-            }
-            n->next->vida = n->next->vida - n->ataque * (n->next->critico * .10);
-            std::cout << n->nombre << " ataca a " << n->next->nombre << std::endl;
-            std::cout << "La vida de " << n->next->nombre << " es de " << n->next->vida << std::endl;
-            if (n->next->vida <= 0) {
-                std::cout << n->next->nombre << " ha sido eliminado\n";
-                n->next = n->next->next;
-            }
-            else {
-                n->vida = n->vida - n->next->ataque * (n->critico * .10);
-                std::cout << n->next->nombre << " ataca a " << n->nombre << std::endl;
-                std::cout << "La vida de " << n->nombre << " es de " << n->vida << std::endl;
-                if (n->vida <= 0) {
-                    std::cout << n->nombre << " ha sido eliminado\n";
-                    n->before->next = n->before->next->next;
-                }
-            }
+        if (n->next->vida <= 0) {
+            n->next == n->next;
         }
-        if (n->nombre == n->next->nombre) {
-            std::cout << n->nombre << " es el ganador";
-            break;
+        if (n->vida < 0) {
+            n = n->next;
+            n->before->vida;
+        }
+        if (n->suerte > n->next->suerte) {
+                n->power = rand() % 51;
+                if (n->power < 11) {
+                    std::cout << n->nombre << " Ha encontrado un artefacto magico, ahora su ataque se duplica\n";
+                    n->ataque * 2;
+                }
+                n->next->vida = n->next->vida - n->ataque * (n->next->critico * .10);
+                std::cout << n->nombre << " ataca a " << n->next->nombre << std::endl;
+                std::cout << "La vida de " << n->next->nombre << " es de " << n->next->vida << std::endl;
+                if (n->next->vida <= 0) {
+                    std::cout << n->next->nombre << " ha sido eliminad@\n";
+                    n->next = n->next->next;
+                }
+                else {
+                    n->vida = n->vida - n->next->ataque * (n->critico * .10);
+                    std::cout << n->next->nombre << " ataca a " << n->nombre << std::endl;
+                    std::cout << "La vida de " << n->nombre << " es de " << n->vida << std::endl;
+                    if (n->vida <= 0) {
+                        std::cout << n->nombre << " ha sido eliminad@\n";
+                        n->before->before->next = n->before->before->next->next;
+                    }
+                }
+                if (n->nombre == n->next->nombre || n->nombre == n->before->nombre) {
+                    std::cout << n->nombre << " ha ganado";
+                    break;
+                }
         }
         n = n->next;
-        }
+    } while (n->vida != 0);
+    if (n->nombre == n->next->nombre || n->nombre == n->before->nombre) {
+        std::cout << n->nombre << " ha ganado";
+        
+    }
     }
 
 
@@ -104,7 +115,7 @@ int main()
     Cesar = new nodo();
     Sebastian = new nodo();
 
-    Iker->ataque = rand() % 51 ;
+    Iker->ataque = 1 + rand() % (51 - 1) ;
     Iker->nombre = "Iker";
     Iker->vida = 100;
     Iker->critico = rand() % 51;
@@ -122,7 +133,7 @@ int main()
     Tadeo->next = Fer;
     Tadeo->before = Iker;
 
-    Fer->ataque = rand() % 51;
+    Fer->ataque = 1 + rand() % (51 - 1);
     Fer->nombre = "Fer";
     Fer->vida = 100;
     Fer->critico = rand() % 51 ;
@@ -131,7 +142,7 @@ int main()
     Fer->next = Arnolfo;
     Fer->before = Tadeo;
 
-    Arnolfo->ataque = rand() % 51 ;
+    Arnolfo->ataque = 1 + rand() % (51 - 1) ;
     Arnolfo->nombre = "Arnolfo";
     Arnolfo->vida = 100;
     Arnolfo->critico = rand() % 51 ;
@@ -140,7 +151,7 @@ int main()
     Arnolfo->next = JuanFer;
     Arnolfo->before = Fer;
 
-    JuanFer->ataque = rand() % 51 ;
+    JuanFer->ataque = 1 + rand() % (51 - 1) ;
     JuanFer->nombre = "JuanFer";
     JuanFer->vida = 100;
     JuanFer->critico = rand() % 51 ;
@@ -149,7 +160,7 @@ int main()
     JuanFer->next = Enrique;
     JuanFer->before = Arnolfo;
 
-    Enrique->ataque = rand() % 51;
+    Enrique->ataque = 1 + rand() % (51 - 1);
     Enrique->nombre = "Enrique";
     Enrique->critico = rand() % 51 ;
     Enrique->vida = 100;
@@ -157,7 +168,7 @@ int main()
     Enrique->next = Daniel;
     Enrique->before = JuanFer;
 
-    Daniel->ataque = rand() % 51 ;
+    Daniel->ataque = 1 + rand() % (51 - 1) ;
     Daniel->nombre = "Daniel";
     Daniel->vida = 100;
     Daniel->critico = rand() % 51 ;
@@ -166,7 +177,7 @@ int main()
     Daniel->next = Betsa;
     Daniel->before = Enrique;
 
-    Betsa->ataque = rand() % 51 ;
+    Betsa->ataque = 1 + rand() % (51 - 1) ;
     Betsa->nombre = "Betsa";
     Betsa->vida = 100;
     Betsa->critico = rand() % 51 ;
@@ -175,7 +186,7 @@ int main()
     Betsa->next = Emma;
     Betsa->before = Daniel;
 
-    Emma->ataque = rand() % 51;
+    Emma->ataque = 1 + rand() % (51 - 1);
     Emma->nombre = "Emma";
     Emma->vida = 100;
     Emma->critico = rand() % 51 ;
@@ -184,7 +195,7 @@ int main()
     Emma->next = Rafa;
     Emma->before = Betsa;
 
-    Rafa->ataque = rand() % 51 ;
+    Rafa->ataque = 1 + rand() % (51 - 1) ;
     Rafa->nombre = "Rafa";
     Rafa->vida = 100;
     Rafa->critico = rand() % 51 ;
@@ -193,7 +204,7 @@ int main()
     Rafa->next = Julio;
     Rafa->before = Emma;
 
-    Julio->ataque = rand() % 51 ;
+    Julio->ataque = 1 + rand() % (51 - 1) ;
     Julio->nombre = "Julio";
     Julio->vida = 100;
     Julio->critico = rand() % 51 ;
@@ -202,7 +213,7 @@ int main()
     Julio->next = Yessica;
     Julio->before = Rafa;
 
-    Yessica->ataque = rand() % 51;
+    Yessica->ataque = 1 + rand() % (51 - 1);
     Yessica->nombre = "Yessica";
     Yessica->vida = 100;
     Yessica->critico = rand() % 51 ;
@@ -211,7 +222,7 @@ int main()
     Yessica->next = Paul;
     Yessica->before = Julio;
 
-    Paul->ataque = rand() % 51 ;
+    Paul->ataque = 1 + rand() % (51 - 1) ;
     Paul->nombre = "Paul";
     Paul->vida = 100;
     Paul->critico = rand() % 51 ;
@@ -220,7 +231,7 @@ int main()
     Paul->next = Pepe;
     Paul->before = Yessica;
 
-    Pepe->ataque = rand() % 51 ;
+    Pepe->ataque = 1 + rand() % (51 - 1) ;
     Pepe->nombre = "Pepe";
     Pepe->vida = 100;
     Pepe->critico = rand() % 51 ;
@@ -229,7 +240,7 @@ int main()
     Pepe->next = Cat;
     Pepe->before = Paul;
 
-    Cat->ataque = rand() % 51;
+    Cat->ataque = 1 + rand() % (51 - 1);
     Cat->nombre = "Cat";
     Cat->vida = 100;
     Cat->critico = rand() % 51 ;
@@ -238,7 +249,7 @@ int main()
     Cat->next = Roberto;
     Cat->before = Pepe;
 
-    Roberto->ataque = rand() % 51 ;
+    Roberto->ataque = 1 + rand() % (51 - 1) ;
     Roberto->nombre = "Roberto";
     Roberto->vida = 100;
     Roberto->critico = rand() % 51 ;
@@ -247,7 +258,7 @@ int main()
     Roberto->next = Ximena;
     Roberto->before = Cat;
 
-    Ximena->ataque = rand() % 51 ;
+    Ximena->ataque = 1 + rand() % (51 - 1) ;
     Ximena->nombre = "Ximena";
     Ximena->vida = 100;
     Ximena->critico = rand() % 51 ;
@@ -256,7 +267,7 @@ int main()
     Ximena->next = Mariana;
     Ximena->before = Roberto;
 
-    Mariana->ataque = rand() % 51;
+    Mariana->ataque = 1 + rand() % (51 - 1);
     Mariana->nombre = "Mariana";
     Mariana->vida = 100;
     Mariana->critico = rand() % 51 ;
@@ -265,7 +276,7 @@ int main()
     Mariana->next = Cesar;
     Mariana->before = Ximena;
 
-    Cesar->ataque = rand() % 51 ;
+    Cesar->ataque = 1 + rand() % (51 - 1) ;
     Cesar->nombre = "Cesar";
     Cesar->vida = 100;
     Cesar->critico = rand() % 51 ;
@@ -274,7 +285,7 @@ int main()
     Cesar->next = Sebastian;
     Cesar->before = Mariana;
 
-    Sebastian->ataque = rand() % 51;
+    Sebastian->ataque = 1 + rand() % (51 - 1);
     Sebastian->nombre = "Sebastian";
     Sebastian->vida = 100;
     Sebastian->critico = rand() % 51 ;
@@ -282,6 +293,7 @@ int main()
     Sebastian->power = rand() % 51;
     Sebastian->next = Iker;
     Sebastian->before = Cesar;
-
+    std::cout << "Los juegos del hambre comienzan\n";
+    
         imprimir(Iker);
 }
